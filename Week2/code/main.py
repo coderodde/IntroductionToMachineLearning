e24b068c071a4ce9f83e2a6d31bea1dc2c2b5e44
Rodion Efremov <rodion.efremov@helsinki.fi>
2015-11-06 15:37:42 +0200

    Presubmission.

--- Week2/code/main.py
+++ Week2/code/main.py
@@ -23,10 +23,6 @@ def jaccard_coefficient(movie_id_a, movie_id_b):
     return 1.0 * len(s) / (len(user_ids_a) + len(user_ids_b) - len(s))
 
 
-def coefficient_compare(a, b):
-    return a[1] > b[1]
-
-
 # Given the movie ID 'target_movie_id' and an integer 'k',
 # find out the list with at most 'k' entries containing tuples
 # (movie_id, Jaccard) that are closest by Jaccard coefficient to
@@ -83,7 +79,7 @@ taxi_passengers = find_k_closest(taxi_driver_id, 5)
 for t in taxi_passengers:
     print movie_names[t[0]], ", coefficient: ", t[1]
 
-print "--- Closest 5 movies to 'Star Wars (1977) ---"
+print "--- Closest 5 movies to 'Star Wars (1977)' ---"
 star_wars_id = reader.give_me_movie_id('Star Wars', movie_dictionary)[0][0]
 star_warriors = find_k_closest(star_wars_id, 5)
 
@@ -92,6 +88,9 @@ for s in star_warriors:
 
 
 def compute_correlation(ratings_a, ratings_b):
+    if len(ratings_a) == 0 or len(ratings_b) == 0:
+        return 0.0
+
     scores_a = [x[2] for x in ratings_a]
     scores_b = [x[2] for x in ratings_b]
 
@@ -149,8 +148,82 @@ def correlation_coefficient(movie_id_a, movie_id_b):
     return compute_correlation(pruned_ratings_a, pruned_ratings_b)
 
 
+def find_k_closest_correlation(target_movie_id, k):
+    map_from_movie_id_to_rating_list = dict()
+
+    # Map each movie to the list of ratings for that movie
+    for r in ratings:
+        movie_id = r[1]
+        if movie_id in map_from_movie_id_to_rating_list:
+            map_from_movie_id_to_rating_list[movie_id].append(r)
+        else:
+            map_from_movie_id_to_rating_list[movie_id] = [r]
+
+    # Build the array of tuples, where each tuple is of the format
+    # (movie_id, correlation coefficient to the target movie).
+    def fast_correlation_coefficient(rating_list_a, rating_list_b):
+        filter_a = set()
+        filter_b = set()
+
+        for r in rating_list_a:
+            filter_a.add(r[0])
+
+        for r in rating_list_b:
+            filter_b.add(r[0])
+
+        common_filter = filter_a.intersection(filter_b)
+
+        pruned_ratings_a = []
+        pruned_ratings_b = []
+
+        for r in rating_list_a:
+            if r[0] in common_filter:
+                pruned_ratings_a.append(r)
+
+        for r in rating_list_b:
+            if r[0] in common_filter:
+                pruned_ratings_b.append(r)
+
+        # Now sort the pruned rating lists by user ID's
+        pruned_ratings_a.sort(key=lambda x: x[0])
+        pruned_ratings_b.sort(key=lambda x: x[0])
+
+        return compute_correlation(pruned_ratings_a, pruned_ratings_b)
+
+    ret = []
+
+    for movie_id in map_from_movie_id_to_rating_list:
+        if movie_id == target_movie_id:
+            continue
+
+        ret.append((movie_id,
+                    fast_correlation_coefficient(map_from_movie_id_to_rating_list[movie_id],
+                                                 map_from_movie_id_to_rating_list[target_movie_id])))
+
+    ret = sorted(ret, key=lambda x: -x[1])
+
+    while len(ret) > k:
+        ret.pop()
+
+    return ret
+
+
 print "--- Correlation coefficient between 'Toy Story' and 'GoldenEye' ---"
 print correlation_coefficient(toy_story_id, golden_eye_id)
 
 print "--- Correlation coefficient between 'Three Colors: Red' and 'Three Color: Blue' ---"
 print correlation_coefficient(red_id, blue_id)
+
+print "--- Closest 5 movies to 'Taxi Driver' by correlation ---"
+taxi_driver_id = reader.give_me_movie_id('Taxi Driver', movie_dictionary)[0][0]
+taxi_passengers = find_k_closest_correlation(taxi_driver_id, 5)
+
+for t in taxi_passengers:
+    print movie_names[t[0]], ", coefficient: ", t[1]
+
+print "--- Closest 5 movies to 'Star Wars (1977)' by correlation ---"
+star_wars_id = reader.give_me_movie_id('Star Wars', movie_dictionary)[0][0]
+star_warriors = find_k_closest_correlation(star_wars_id, 5)
+
+for s in star_warriors:
+    print movie_names[s[0]], ", coefficient: ", s[1]
