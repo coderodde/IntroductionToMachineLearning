2c0919478f4b7c2ad2f749922bbdfe9fcf55bd51
Rodion Efremov <rodion.efremov@helsinki.fi>
2015-11-27 15:09:16 +0200

    4a done.

--- Week5/Code/013593012.py
+++ Week5/Code/013593012.py
@@ -22,14 +22,37 @@ def get_y_coordinates(x_coordinates):
 
     return np.array(arr)
 
+
+def det_coef(sampled_values, fitted_values):
+    ave = np.mean(sampled_values)
+    upper_sum = 0
+    lower_sum = 0
+
+    for i in range(0, len(sampled_values)):
+        upper_sum += (sampled_values[i] - fitted_values[i]) ** 2
+        lower_sum += (sampled_values[i] - ave) ** 2
+
+    return upper_sum / lower_sum
+
+
+def process(x_coordinates, k):
+    space = np.linspace(-3.0, 3.0, 1000)
+    y_coordinates = get_y_coordinates(x_coordinates)
+    p = np.polyfit(x_coordinates, y_coordinates, k)
+    v = np.polyval(p, space)
+    plt.plot(x_coordinates, f(x_coordinates), 'ro')
+    plt.plot(space, v)
+    plt.axis([-3, 3, -6, 4])
+    plt.title('K = ' + str(k) + ", R^2 = " + str(det_coef(y_coordinates, np.polyval(p, x_coordinates))))
+    plt.show()
+
+
 def main():
     x_coordinates = get_x_coordinates()
-    p = np.polyfit(x_coordinates, get_y_coordinates(x_coordinates), 10)
-    v = np.polyval(p, np.linspace(-3.0, 3.0, 1000))
-    plt.plot(x_coordinates, f(x_coordinates), 'ro')
-    plt.plot(np.linspace(-3.0, 3.0, 1000), v)
-    plt.axis([-3, 3, -6, 3])
-    plt.show()
+
+    for k in range(0, 11):
+        process(x_coordinates, k)
+
 
 if __name__ == "__main__":
     main()
