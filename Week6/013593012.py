1be3ba81501dd7cba6e1d3ffab277ce51ecf17ec
Rodion Efremov <rodion.efremov@helsinki.fi>
2015-12-03 15:53:31 +0200

    ...

--- Week6/013593012.py
+++ Week6/013593012.py
@@ -22,7 +22,6 @@ yTrain = y[0:30000]
 yTest  = y[30000:60000]
 
 
-
 def my_info():
     return student_ID
 
@@ -32,33 +31,35 @@ def one_vs_all():
     # w_classes is an array of length 10 holding the w-vector for each class.
     # The ith vector corresponds to the class 'i'.
     w_classes = [np.array([0 for i in range(len(XTrain[0]))]) for cls in range(10)]
-    best_scores = [0 for cls in range(10)]
+    w_classes_pockets = [np.array([0 for i in range(len(XTrain[0]))]) for cls in range(10)]
 
-    for i in range(len(XTrain)):
-        X = XTrain[i]
-        y = yTrain[i]
 
-        best_class = 0
-        best_cost  = np.dot(w_classes[0], X)
+    for i in range(1):
+        for i in range(len(XTrain)):
+            X = XTrain[i]
+            actual_class = yTrain[i]
+            best_class = 0
+            best_cost = np.dot(w_classes[0], X)
 
-        for cls in range(1, 10):
-            current_cost = np.dot(w_classes[cls], X)
-            if best_cost < current_cost:
-                best_cost = current_cost
-                best_class = cls
+            for cls in range(1, 10):
+                current_cost = np.dot(w_classes[cls], X)
 
-        if best_class != y:
-            w_classes[best_class] -= X
-            w_classes[y] += X
+                if best_cost < current_cost:
+                    best_cost = current_cost
+                    best_class = cls
+
+            if best_class != actual_class:
+                w_classes[best_class] -= X
+                w_classes[actual_class] += X
 
     #### CLASSIFY ####
     # Create confusion matrix:
-    confusion_matrix = [[0 for i in range(10)] for j in range(10)]
+    confusion_matrix = [[0 for i in range(10)] for i in range(10)]
 
     # Iterate over the test data.
     for i in range(len(XTest)):
         X = XTest[i]
-        y = yTest[i]
+        actual_class = yTest[i]
 
         best_score = 0
         best_class = -1
@@ -71,24 +72,24 @@ def one_vs_all():
                 best_class = cls
 
         # Update confusion matrix:
-        confusion_matrix[best_class][y] += 1
+        confusion_matrix[best_class][actual_class] += 1
     # Done iterating over the test data.
     return confusion_matrix
 
-matrix = one_vs_all()
+#matrix = one_vs_all()
 
-for i in range(len(matrix)):
-    print(matrix[i])
+#for i in range(len(matrix)):
+#    print(matrix[i])
 
-sum = 0
+#sum = 0
 
-for i in range(len(matrix)):
-    sum += matrix[i][i]
+#for i in range(len(matrix)):
+#    sum += matrix[i][i]
 
-print("Correct:", sum)
+#print("Correct", sum)
 
 def all_vs_all():
-    all_vs_all_conf_matrix = ''
+    all_vs_all_conf_matrix = [[1 for i in range(10)] for j in range(10)]
     return all_vs_all_conf_matrix
 
 
@@ -101,6 +102,7 @@ def main():
     results += np.array_str(np.diagonal(all_vs_all()))
     print results + '\t\t'
 
-# Uncomment me!
-#if __name__ == '__main__':
-    #main()
+
+
+if __name__ == '__main__':
+    main()
